de5b0fb23f770d605e399bae4600378098d36bb8
mhuggi <emil.e.salmi@gmail.com>
2020-06-12 14:35:03 +0300

    init anekdootit

---
+++ osa1/anekdootit/src/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length + 1).join('0').split('').map(parseFloat))
+
+  const newAnecdote = () => {
+    setSelected(Math.floor(Math.random() * Math.floor(anecdotes.length)))
+  }
+  const vote = () => {
+    const newPoints = [...points]
+    newPoints[selected] += 1
+    setPoints(newPoints)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button onClick={newAnecdote} text='next anecdote' />
+      <Button onClick={vote} text='Vote' />
+
+      <h1>Anecdote with the most votes</h1>
+      <p>{props.anecdotes[points.indexOf(Math.max(...points))]}</p>
+
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
