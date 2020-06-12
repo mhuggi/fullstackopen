a64f3649a3af560701471536d98abf02601fe1e4
mhuggi <emil.e.salmi@gmail.com>
2020-06-12 13:00:24 +0300

    finished unicafe

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,19 +1,74 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const StatisticsLine = (props) => {
+  return (
+    <tbody>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
+    </tbody>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  } else {
+    return (
+      <table>
+        <StatisticsLine text='good' value={props.good} />
+        <StatisticsLine text='neutral' value={props.neutral} />
+        <StatisticsLine text='bad' value={props.bad} />
+        <StatisticsLine text='all' value={props.all} />
+        <StatisticsLine text='average' value={(props.good - props.bad) / props.all} />
+        <StatisticsLine text='positive' value={(props.good / props.all) * 100 + "%"} />
+      </table>
+
+    )
+  }
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={handleGood} text='good' />
+      <Button onClick={handleNeutral} text='neutral' />
+      <Button onClick={handleBad} text='bad' />
+      <h1>Statistics</h1>
+
+      <Statistics good={good} neutral={neutral} bad={bad} all={good + neutral + bad} />
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
