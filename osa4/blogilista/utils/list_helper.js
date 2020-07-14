8a880e4998351b3c6373a1658bf16615fdbea31a
mhuggi <emil.e.salmi@gmail.com>
2020-07-14 15:21:11 +0300

    4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,14 +3,33 @@ const dummy = (blogs) => {
   }
   
 const totalLikes = (blogs) => {
-    console.log(blogs)
     let likes = 0
     for (i = 0; i<blogs.length; i++) {
         likes += blogs[i].likes
     }
     return likes
 }
+
+const favoriteBlog = (blogs) => {
+    let blog = {
+        'title' : 'Dummy',
+        'Author' : 'Dummy',
+        'likes' : 0
+    }
+    
+    for (i=0;i<blogs.length;i++) {
+        if(blog.likes < blogs[i].likes) {
+            blog = blogs[i]
+        }
+    }
+    return {
+        'title' : blog.title,
+        'author' : blog.author,
+        'likes' : blog.likes
+    }
+}
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
