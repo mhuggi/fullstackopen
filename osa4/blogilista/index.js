fc342bcadf177b61285f5855d0fdc7d3ab319648
mhuggi <emil.e.salmi@gmail.com>
2020-07-14 12:20:13 +0300

    4.2

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,4 +1,16 @@
-require('dotenv').config()
+const app = require('./app') // varsinainen Express-sovellus
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
+
+
+/*require('dotenv').config()
 
 const http = require('http')
 const express = require('express')
@@ -44,3 +56,4 @@ const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+*/
