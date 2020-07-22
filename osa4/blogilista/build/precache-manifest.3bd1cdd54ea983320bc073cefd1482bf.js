d662c107b548add7da64e80787292e3c8619eea7
mhuggi <emil.e.salmi@gmail.com>
2020-07-22 14:31:35 +0300

    5.1

---
+++ osa4/blogilista/build/precache-manifest.3bd1cdd54ea983320bc073cefd1482bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71797373dc1c22b1fc709dbf3b0f4d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f597f22a07dbd553533",
+    "url": "/static/js/2.2b41f33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b41f33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9af79e8fff98462f0d4",
+    "url": "/static/js/main.bcba05fe.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
