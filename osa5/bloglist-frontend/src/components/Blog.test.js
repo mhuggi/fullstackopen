4af34d88bbcf9b5a4aba4238268d5919923902d9
mhuggi <emil.e.salmi@gmail.com>
2020-07-29 13:09:06 +0300

    5.13

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -4,51 +4,58 @@ import { render, fireEvent } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
+    
 
-
-test('Only show Title and Author', () => {
+describe('<Blog />', () => {
+    let component
     const blog = {
         title: 'Component testing is done with react-testing-library',
         author: 'Testman',
-        url: 'test.com'
+        url: 'test.com',
+        user: {
+            username: "eme",
+            name: "Emil Salmi"
+        }
     }
-    const component = render(
-        <Blog blog={blog} />
-    )
-    expect(component.container).toHaveTextContent(
-        'Component testing is done with react-testing-library'
-    )
-    expect(component.container).toHaveTextContent(
-        'Testman'
-    )
-    const url = component.queryByText(
-        'test.com'
-    )
-    expect(url).toBeNull()
-    const likes = component.queryByText(
-        'likes'
-    )
-    expect(likes).toBeNull()
-})
+    const loggedUser = {
+        username: "eme",
+        name: "Emil Salmi"
+    }
 
-test('Show likes and url after pressing view', () => {
-    const blog = {
-        title: 'Component testing is done with react-testing-library',
-        author: 'Testman',
-        url: 'test.com'
-    }
     beforeEach(() => {
-    component = render(
-        <Blog blog={blog} />
-    )
-    
-    const button = component.getByText('view')
-    fireEvent.click(button)
-    
-    expect(component.container).toHaveTextContent('test.com')
-    expect(component.container).toHaveTextContent('likes')
+        localStorage.setItem('loggedBlogUser', loggedUser)
+        component = render(
+                <Blog blog={blog} />
+        )
+    })
+    test('Only show Title and Author', () => {
+        expect(component.container).toHaveTextContent(
+            'Component testing is done with react-testing-library'
+        )
+        expect(component.container).toHaveTextContent(
+            'Testman'
+        )
+        const url = component.queryByText(
+            'test.com'
+        )
+        expect(url).toBeNull()
+        const likes = component.queryByText(
+            'likes'
+        )
+        expect(likes).toBeNull()
+    })
+
+    test('Show likes and url after pressing view', () => {
+        const li = component.container.querySelector('li')
+        console.log(prettyDOM(li))
+        
+        const button = component.getByText('View')
+        fireEvent.click(button)
+
+        expect(component.container).toHaveTextContent('test.com')
+        expect(component.container).toHaveTextContent('likes')
 
 
-})
+    })
 
 })
